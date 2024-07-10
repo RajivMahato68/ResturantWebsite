9a1eefd70313194b73e5206122b13454b9d0f256
rajipmahato68 <rajipmahato68@gmail.com>
2024-07-10 16:05:01 +0545

    FooterCompleted

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,110 +1,93 @@
 import React from 'react';
-
-import { Mail, MapPin, Facebook, Instagram, Phone } from "lucide-react";
-import momoStreet from "../../assets/logo.png";
-
+import Logo from "../../assets/logo.png";
+import { MdOutlineEmail } from "react-icons/md";
+import { SlLocationPin } from "react-icons/sl";
+import { SlSocialFacebook } from "react-icons/sl";
+import { CiInstagram } from "react-icons/ci";
+import { FiPhone } from "react-icons/fi";
 
 const Footer = () => {
   const quickLinks = [
-    { label: 'Home', href: '/' },
-    { label: 'Menu', href: '/menu' },
-    { label: 'Contact Us', href: '/contact' },
-    { label: 'About', href: '/about' },
-    { label: 'Blog', href: '/blog' },
-    { label: 'Order Online', href: '/order' },
+    { label: 'Home', href: '#' },
+    { label: 'Menu', href: '#' },
+    { label: 'Contact Us', href: '#' },
+    { label: 'About', href: '#' },
+    { label: 'Blog', href: '#' },
+    { label: 'Order Online', href: '#' },
+  ];
+
+  const schedule = [
+    { day: 'Sun - Fri:', hours: '9:00AM-9:00PM' },
+    { day: 'Saturday:', hours: '1:00PM - 10:00PM' },
+  ];
+
+  const stayUpToDate = [
+    { label: <SlLocationPin className='w-[18px] h-[18px]' />, value: 'Kathmandu,Nepal' },
+    { label: <MdOutlineEmail className='w-[18px] h-[18px]' />, value: 'Roel.Rodriguez@gmail.com' },
+    { label: <FiPhone className='w-[18px] h-[18px]' />, value: '+977-9898989890' },
   ];
 
   return (
-    <footer className="bg-[#232323] text-white py-10">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-full  lg:w-1/4 p-6 flex flex-col items-center lg:ml-10">
-            <img
-              className="lg:h-16 lg:w-16 h-12 w-12 object-fit object-cover lg:ml-9"
-              src={momoStreet}
-              alt="MoMo Street"
-            />
-            <p className="text-white lg:text-center text-justify text-xs mt-6 lg:ml-12">
-              Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              vulputate libero et velit interdum, ac aliquet odio mattis. Vorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
-              libero et velit interdum, ac aliquet odio mattis.
+    <footer className="bg-[#232323] text-white py-10 px-4 lg:px-[9rem]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:justify-between">
+          <div className="lg:w-1/4 mb-6 lg:mb-0  text-center">
+            <img src={Logo} alt="logo" className="h-[70px] w-[70px] mx-auto  lg:ml-[6rem] object-fit" />
+            <p className="mt-5 text-[12px] text-[#c7bfb7] ">
+              Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
             </p>
-            <div className="flex lg:pr-24 items-center space-x-10 lg:ml-20 mt-10 ">
-              <Facebook className="text-xl cursor-pointer " />
-              <Instagram className="text-xl cursor-pointer" />
-              <Mail className="text-xl cursor-pointer" />
+            <div className="mt-[3rem] gap-8 lg:justify-center justify-center flex">
+              <SlSocialFacebook className='w-[20px] h-[20px]' />
+              <CiInstagram className='w-[20px] h-[20px]' />
+              <MdOutlineEmail className='w-[20px] h-[20px]' />
             </div>
           </div>
-
-          <div className="flex flex-col lg:flex-row lg:space-x-12  lg:mt-0">
-            <div className="w-full lg:w-1/4 p-6  ">
-              <h3 className="text-2xl font-normal text-white underline-bold underline decoration-[#f38221] decoration-2 underline-offset-4 font-sans-serif mb-4 mt-1">
-                Quick Links
-              </h3>
-              <ul>
-                {quickLinks.map((link) => (
-                  <li key={link.label} className='mb-2 lg:ml-5 '>
-                    <a href={link.href} className="text-white hover:text-white font-plus-jakarta-sans text-button-text font-medium leading-button-text tracking-button-text text-left "
-            >
-                      {link.label}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+          <div className="lg:w-1/4 mb-6 lg:mb-0 lg:ml-[3rem] text-center">
+            <h5 className="mb-4 border-b-2 border-[#F38220] inline-block font-plus-jakarta-sans font-semibold text-[22px] text-center lg:text-left ">Quick Links</h5>
+            <ul className="list-none text-[#d8d2d2] lg:text-left lg:ml-[4rem]">
+              {quickLinks.map((link, index) => (
+                <li key={index} className="mb-2">
+                  <a href={link.href} className="">
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="lg:w-1/4 mb-6 lg:mb-0 text-center">
+            <h5 className="mb-4  border-b-2 border-[#F38220] inline-block font-plus-jakarta-sans font-semibold text-[22px] text-center ">Schedule</h5>
+            <ul className="list-none text-[#d8d2d2]  lg:mr-[1rem]">
+              {schedule.map((scheduleItem, index) => (
+                <li key={index} className="mb-5 lg:text-center ">
+                  <span className="text-[16px] ">{scheduleItem.day}</span>
+                  <span className="text-[16px] block">{scheduleItem.hours}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="lg:w-1/4">
+            <h5 className="mb-4 border-b-2 border-[#F38220] inline-block font-plus-jakarta-sans font-semibold text-[22px] text-center ml-[4rem] lg:ml-[3rem] ">Stay up to date</h5>
+            <div className="flex lg:flex-row items-center lg:items-start space-x-2">
+              <input
+                className="w-[200px] lg:w-[187px] h-[46px] mb-5 border border-white text-black px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="email"
+                placeholder="Email address"
+              />
+              <button
+                type="button"
+                className="border border-[#F38220] w-full lg:w-[65px] h-[46px] bg-white px-3 py-2 text-sm font-semibold text-[#F38220] shadow-sm hover:bg-red-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 text-[12px] flex items-center justify-center mb-5 "
+              >
+                Submit
+              </button>
             </div>
-
-            <div className="w-full lg:w-1/4 p-4 text-center lg:mb-12 gap-6">
-              <h3 className="text-2xl font-normal text-white underline-bold underline decoration-[#f38221] decoration-2 underline-offset-4 font-sans-serif ml-10 mt-3 mb-4">
-                Schedule
-              </h3>
-              <ul className="text-white-400 mt-2 font-sans-serif">
-                <li className="mb-1 lg:ml-9 text-white">
-                  Sun - Fri:
+            <ul className="text-[#c7bfb7] text-[12px] text-center">
+              {stayUpToDate.map((item, index) => (
+                <li key={index} className="flex mb-3  text-center ml-[4rem] lg:ml-[0rem] lg:justify-start">
+                  <span className="mr-2">{item.label}</span>
+                  <span>{item.value}</span>
                 </li>
-                <li className="mb-4 ml-9 text-white">
-                  9:00AM - 9:00PM
-                </li>
-                <li className="mb-1 lg:ml-9  text-white">
-                  Sun - Fri:
-                </li>
-                <li className="mb-4 ml-8 text-white">
-                  9:00AM - 9:00PM
-                </li>
-              </ul>
-            </div>
-
-            <div className="w-full lg:w-1/4 p-3 mt-4 gap-6">
-              <h3 className="text-2xl font-normal text-white underline-bold underline decoration-[#f38221] decoration-2 underline-offset-4 font-sans-serif mb-4 ml-7">
-                Stay up to date
-              </h3>
-              <form className="flex flex-col md:flex-row mb-2 text-white ">
-      <input
-        type="email"
-        placeholder="Email address"
-        className="bg-white text-gray-400 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 w-full md:w-[250px] h-[46px] p-4"
-      />
-      <button
-        type="submit"
-        className="bg-white text-[#f38220] hover:text-white border border-[#f38220] rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full md:w-[140px] md:ml-2 hover:border-[#e9252f] hover:bg-[#E9252F] text-sm h-[46px]"
-      >
-        Submit
-      </button>
-    </form>
-              <div className="text-white space-x-1 flex items-center mt-3">
-                <MapPin className="text-[#ffffff]" />
-                <h1 className="font-jakarta text-sm font-normal leading-6 text-left">Kathmandu,Nepal</h1>
-              </div>
-              <div className="text-white space-x-1 flex items-center mt-3">
-                <Mail className="text-[#ffffff]" />
-                <h1 className="font-jakarta text-sm font-normal leading-6 text-left">MoMo@gmail.com</h1>
-              </div>
-             <div className="text-white space-x-1 flex items-center mt-4">
-  <Phone className="text-[#ffffff]" />
-  <h1 className="font-jakarta text-sm font-normal leading-6 text-left">+9778000032099</h1>
-</div>
-
-            </div>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
